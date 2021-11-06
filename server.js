9e802d81dc242ce5b6788391237cb5870d714d4e
David Lavigne <dlavigne@zipwhip.com>
2021-11-06 12:19:17 -0700

    Update server.js

--- server.js
+++ server.js
@@ -50,13 +50,7 @@ app.post("/api/updateUserProfile", checkJwt, function(req, res) {
     }
   });
 
-  console.log(JSON.stringify(decycle(res, undefined)));
-
-  var oID = req.body.params;
-  var oOrders = req.body.metadata;
-
-  console.log("ID: " + JSON.stringify(oID));
-  console.log("Orders: " + JSON.stringify(oOrders));
+  console.log("TEST: " + JSON.stringify(decycle(res, undefined)));
   
   mgmt.users.updateUserMetadata(req.body.params.id, req.body.metadata.orders, function (err, user) {
     if (err) {
