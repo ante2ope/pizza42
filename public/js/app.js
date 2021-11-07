6207cc085410bce7ebde19ce1d761ba5b9202d46
David Lavigne <dlavigne@zipwhip.com>
2021-11-07 11:21:23 -0800

    Update app.js

--- public/js/app.js
+++ public/js/app.js
@@ -40,7 +40,8 @@ const configureClient = async () => {
   auth0 = await createAuth0Client({
     domain: config.domain,
     client_id: config.clientId,
-    audience: config.audience
+    audience: config.audience,
+    scope: "read:user_profile, write:user_profile"
   });
 };
 
